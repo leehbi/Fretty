dd9ec69be93a96663ee581d8c0552e93b9be82fe
leehbi <mail@leehawthorn.co.uk>
2020-04-26 16:14:28 +0100

    reorganised files to make game additions easier

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "bootstrap/dist/css/bootstrap.css";
-import GuessNoteGame from "./components/GuessNoteGame";
+import GuessNoteGame from "./components/GuessNoteGame/GuessNoteGame";
 
 class App extends React.Component {
   render() {
